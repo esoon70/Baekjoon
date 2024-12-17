36d329de80fb08cfbf8450be39addbfe077381af
Park Chanmin <68807941+esoon70@users.noreply.github.com>
2024-12-17 16:25:40 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    int A;
+    scanf("%d", &A);
+    if (A == 0) {
+        printf("YONSEI");
+    } else {
+        printf("Leading the Way to the Future");
+    }
+} 
