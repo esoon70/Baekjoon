065ce9db90ff129dfeec01f7844b41bf2dadbd4d
Park Chanmin <68807941+esoon70@users.noreply.github.com>
2024-12-17 16:19:02 +0900

    Update A／B.cc

--- 백준/Bronze/1008. A／B/A／B.cc
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -1,7 +1,10 @@
 #include <iostream>
+using namespace std;
 
 int main() {
     double A, B;
-    scanf("%lf %lf", &A, &B);
-    printf("%.10lf", A / B);
+    cin >> A >> B;
+    cout << fixed;
+    cout.precision(9);
+    cout << A / B;
 }
