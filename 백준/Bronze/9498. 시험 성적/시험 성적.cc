4d3ca3715a5786382f3a43ff73f613a1bb53a9c3
Park Chanmin <68807941+esoon70@users.noreply.github.com>
2024-12-18 15:04:18 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int Score;
+    cin >> Score;
+    if (Score >= 90) {
+        cout << "A";
+    } else if (Score >= 80) {
+        cout << "B";
+    } else if (Score >= 70) {
+        cout << "C";
+    } else if (Score >= 60) {
+        cout << "D";
+    } else {
+        cout << "F";
+    }
+}
