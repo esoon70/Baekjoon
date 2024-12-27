ad98a26d3d960e03379dee28d85b3922f78da900
Park Chanmin <68807941+esoon70@users.noreply.github.com>
2024-12-27 14:07:56 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -4,12 +4,15 @@ using namespace std;
 
 int main() {
     int A, C;
-    int count = 0;
+    long long count = 0;
+    long long M = 1234567891;
+    long long r = 1;
     string B;
     cin >> A >> B;
     for (int i = 0; i < B.length(); i++) {
         C = int(B[i]) - 96;
-        count += C * pow(31, i);
+        count = (count + C * r) % M;
+        r = r * 31 % M;
     }
     cout << count;
 }
