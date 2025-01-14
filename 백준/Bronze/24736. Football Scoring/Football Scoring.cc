2f5f4795c39d071c339b464f5e90f69279f01664
Park Chanmin <68807941+esoon70@users.noreply.github.com>
2025-01-14 13:16:29 +0900

    [Bronze V] Title: Football Scoring, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24736. Football Scoring/Football Scoring.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T, F, S, P, C;
+    for (int i = 0; i < 2; i++) {
+        cin >> T >> F >> S >> P >> C;
+        cout << T * 6 + F * 3 + S * 2 + P * 1 + C * 2 << '\n';
+    }
+}
