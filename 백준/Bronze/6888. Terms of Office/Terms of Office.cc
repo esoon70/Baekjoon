1ee0c7d0516776b744a74c3532ce8ed7f00b44c9
Park Chanmin <68807941+esoon70@users.noreply.github.com>
2025-02-27 15:58:33 +0900

    [Bronze IV] Title: Terms of Office, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6888. Terms of Office/Terms of Office.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int X, Y;
+    cin >> X >> Y;
+    while (X <= Y) {
+        cout << "All positions change in year " << X << '\n';
+        X += 60;
+    }
+}
